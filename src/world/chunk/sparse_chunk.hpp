8cab05e68e0ce55936f21e0372b9894e67668f2a
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2026-07-17 13:32:15 +0300

    Made face masking algorithm for sparse chunk encoding

--- src/world/chunk/sparse_chunk.hpp
+++ src/world/chunk/sparse_chunk.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm> // IWYU pragma: keep
+#include <array>
 #include <utils/enums.hpp>
 #include "config.hpp"
 
@@ -13,4 +14,44 @@ struct SparseChunk {
     std::vector<sparseEncoding> blockArray{};
 
     setBlockStatus setBlock(ConstrainedVec3<CHUNK_SIZE> coords, uint16_t blockID);
+
+    std::array<bool, CHUNK_SIZE * CHUNK_SIZE> faceMask(cardinalDirection dir) {
+        std::array<bool, CHUNK_SIZE * CHUNK_SIZE> output = {};
+
+        for (const auto& encoding : blockArray) {
+            if (encoding.blockID == 0) {
+                break;
+            }
+            for (const auto& coords : encoding.coordinates) {
+                switch (dir) {
+                case cardinalDirection::North:
+                    if (coords.z != 0) continue;
+                    output[coords.x + CHUNK_SIZE * coords.y] = true;
+                    break;
+                case cardinalDirection::East:
+                    if (coords.x != CHUNK_SIZE) continue;
+                    output[coords.z + CHUNK_SIZE * coords.y] = true;
+                    break;
+                case cardinalDirection::South:
+                    if (coords.z != CHUNK_SIZE) continue;
+                    output[coords.x + CHUNK_SIZE * coords.y] = true;
+                    break;
+                case cardinalDirection::West:
+                    if (coords.x != 0) continue;
+                    output[coords.z + CHUNK_SIZE * coords.y] = true;
+                    break;
+                case cardinalDirection::Up:
+                    if (coords.y != CHUNK_SIZE) continue;
+                    output[coords.x + CHUNK_SIZE * coords.z] = true;
+                    break;
+                case cardinalDirection::Down:
+                    if (coords.y != 0) continue;
+                    output[coords.x + CHUNK_SIZE * coords.z] = true;
+                    break;
+                }
+            }
+        }
+
+        return output;
+    }
 };
