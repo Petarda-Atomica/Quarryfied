68233f0f5bdd690ee38fa4c1bdd0c99580179085
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2026-07-17 16:51:21 +0300

    Moved sparse chunk face masking from header to source

--- src/world/chunk/sparse_chunk.cpp
+++ src/world/chunk/sparse_chunk.cpp
@@ -43,3 +43,43 @@ setBlockStatus SparseChunk::setBlock(chunkCoord coords, uint16_t blockID) {
 
     return setBlockStatus::Ok;
 }
+
+std::array<bool, CHUNK_SIZE * CHUNK_SIZE> SparseChunk::faceMask(cardinalDirection dir) {
+    std::array<bool, CHUNK_SIZE * CHUNK_SIZE> output = {};
+
+    for (const auto& encoding : blockArray) {
+        if (encoding.blockID == 0) {
+            break;
+        }
+        for (const auto& coords : encoding.coordinates) {
+            switch (dir) {
+            case cardinalDirection::North:
+                if (coords.z != 0) continue;
+                output[coords.x + CHUNK_SIZE * coords.y] = true;
+                break;
+            case cardinalDirection::East:
+                if (coords.x != CHUNK_SIZE) continue;
+                output[coords.z + CHUNK_SIZE * coords.y] = true;
+                break;
+            case cardinalDirection::South:
+                if (coords.z != CHUNK_SIZE) continue;
+                output[coords.x + CHUNK_SIZE * coords.y] = true;
+                break;
+            case cardinalDirection::West:
+                if (coords.x != 0) continue;
+                output[coords.z + CHUNK_SIZE * coords.y] = true;
+                break;
+            case cardinalDirection::Up:
+                if (coords.y != CHUNK_SIZE) continue;
+                output[coords.x + CHUNK_SIZE * coords.z] = true;
+                break;
+            case cardinalDirection::Down:
+                if (coords.y != 0) continue;
+                output[coords.x + CHUNK_SIZE * coords.z] = true;
+                break;
+            }
+        }
+    }
+
+    return output;
+}
