d2a8cc4bf66d0d50538f676c87d468a3a2438003
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2026-07-17 21:24:08 +0300

    Wrote meshing logic for raw chunks

--- src/world/chunk/raw_chunk.cpp
+++ src/world/chunk/raw_chunk.cpp
@@ -1,4 +1,5 @@
 #include "raw_chunk.hpp"
+#include "world/chunk/config.hpp"
 
 setBlockStatus RawChunk::setBlock(chunkCoord coords, uint16_t blockID) {
     blockArray[coords.flatten()] = blockID;
@@ -48,4 +49,150 @@ std::array<bool, CHUNK_SIZE * CHUNK_SIZE> RawChunk::faceMask(cardinalDirection d
     }
 
     return output;
+}
+
+void RawChunk::mesh(std::vector<GPU::CubeFace>& faces, std::vector<GPU::DrawArraysIndirectCommand>& cmds, regionCoord regionCoord, std::array<std::array<bool, CHUNK_SIZE * CHUNK_SIZE>, 6> neighboursFaceMask, BlocksManager* blocksManager) {
+    // Delete old data
+    faces.clear();
+    cmds.clear();
+
+    // Make an array to hold what we are going to draw
+    ankerl::unordered_dense::map<uint16_t, std::vector<GPU::CubeFace>> drawMap;
+
+    // Loop through all blocks
+    for (int x = 0; x < CHUNK_SIZE; x++) {
+        for (int y = 0; y < CHUNK_SIZE; y++) {
+            for (int z = 0; z < CHUNK_SIZE; z++) {
+                // Get the textures of the block
+                auto textures = blocksManager->getTextures(blockArray[chunkCoord(x, y, z).flatten()]);
+
+                // Mesh north face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (z == 0) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::North)][x + CHUNK_SIZE * y];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x, y, z-1).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x;
+                    face.y = regionCoord.y;
+                    face.z = regionCoord.z - 0.5;
+                    face.setOrientation(cardinalDirection::North);
+                    drawMap[textures.north].emplace_back(face);
+                }
+
+                // Mesh east face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (x == CHUNK_SIZE) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::East)][z + CHUNK_SIZE * y];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x+1, y, z).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x + 0.5;
+                    face.y = regionCoord.y;
+                    face.z = regionCoord.z;
+                    face.setOrientation(cardinalDirection::East);
+                    drawMap[textures.east].emplace_back(face);
+                }
+
+                // Mesh south face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (z == CHUNK_SIZE) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::South)][x + CHUNK_SIZE * y];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x, y, z+1).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x;
+                    face.y = regionCoord.y;
+                    face.z = regionCoord.z + 0.5;
+                    face.setOrientation(cardinalDirection::South);
+                    drawMap[textures.south].emplace_back(face);
+                }
+
+                // Mesh west face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (x == 0) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::West)][z + CHUNK_SIZE * y];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x-1, y, z).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x - 0.5;
+                    face.y = regionCoord.y;
+                    face.z = regionCoord.z;
+                    face.setOrientation(cardinalDirection::West);
+                    drawMap[textures.west].emplace_back(face);
+                }
+
+                // Mesh top face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (y == CHUNK_SIZE) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::Up)][x + CHUNK_SIZE * z];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x, y+1, z).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x;
+                    face.y = regionCoord.y + 0.5;
+                    face.z = regionCoord.z;
+                    face.setOrientation(cardinalDirection::Up);
+                    drawMap[textures.top].emplace_back(face);
+                }
+
+                // Mesh bottom face
+                if ([this, x, y, z, neighboursFaceMask]() {
+                    // Check if at the edge of a chunk and visible
+                    if (y == 0) {
+                        return !neighboursFaceMask[static_cast<int>(cardinalDirection::Down)][x + CHUNK_SIZE * z];
+                    }
+
+                    // Check if in chunk and visible
+                    return !static_cast<bool>(blockArray[chunkCoord(x, y-1, z).flatten()]);
+                }()) {
+                    GPU::CubeFace face;
+                    face.x = regionCoord.x;
+                    face.y = regionCoord.y - 0.5;
+                    face.z = regionCoord.z;
+                    face.setOrientation(cardinalDirection::Down);
+                    drawMap[textures.bottom].emplace_back(face);
+                }
+            }
+        }
+    }
+
+    // Pack into draw calls
+    uint32_t instanceOffset = 0;
+    for (auto& [textureID, val] : drawMap) {
+        GPU::DrawArraysIndirectCommand cmd;
+        cmd.count = 4;
+        cmd.instanceCount = (uint32_t)val.size();
+        cmd.first = 0;
+        cmd.baseInstance = instanceOffset;
+        cmds.emplace_back(cmd);
+
+        instanceOffset += (uint32_t)val.size();
+
+        faces.insert(faces.end(), std::make_move_iterator(val.begin()), std::make_move_iterator(val.end()));
+    }
+
+    // Shrink, just in case
+    faces.shrink_to_fit();
+    cmds.shrink_to_fit();
 }
--- src/world/chunk/raw_chunk.hpp
+++ src/world/chunk/raw_chunk.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <utils/enums.hpp>
+#include <graphics/structs.hpp>
 #include "config.hpp"
 #include <array>
 #include <cstdint>
+#include <blocks.hpp>
+#include <ankerl/unordered_dense.h>
 
 class RawChunk {
     std::array<uint16_t, FLATTENED_CHUNK_SIZE> blockArray;
@@ -11,4 +14,6 @@ class RawChunk {
     setBlockStatus setBlock(chunkCoord coords, uint16_t blockID);
 
     std::array<bool, CHUNK_SIZE * CHUNK_SIZE> faceMask(cardinalDirection dir);
+
+    void mesh(std::vector<GPU::CubeFace>& faces, std::vector<GPU::DrawArraysIndirectCommand>& cmds, regionCoord regionCoord, std::array<std::array<bool, CHUNK_SIZE * CHUNK_SIZE>, 6> neighboursFaceMask, BlocksManager* blocksManager);
 };
